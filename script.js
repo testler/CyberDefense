68b9560e6a84a6432385a234bbda30785f9ae21a
testler <joshgarst@me.com>
2022-04-27 20:09:47 -0500

    main backend set, text intro in progress

---
+++ script.js
@@ -0,0 +1,35 @@
+class Towers{
+    Constructor(){
+        
+    }
+
+}
+class Enemies{
+    constructor(){
+
+    }
+}
+class GameSesion{
+    constructor(){
+        this.username = "";
+    }
+    generateLevel(level){
+
+    }
+    gameStart(){
+        let introBox = document.createElement("dialog");
+        document.createTextNode("This is new.");
+        // introBox.outerHTML ="<dialog open>test</dialog>";
+        console.log("getting here");
+
+    }
+    gameLevelsStart(){
+
+    }
+    winnerScreen(){
+
+    }
+}
+let game1 = new GameSesion();
+game1.gameStart();
+console.log("script loaded");
