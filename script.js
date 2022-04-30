f443f0f5bab54cdef50b4c54ebf24a38b90cefdf
testler <joshgarst@me.com>
2022-04-30 17:18:58 -0500

    level 1 can generate

--- script.js
+++ script.js
@@ -16,8 +16,108 @@ class GameSesion{
         this.quadrantArray = [];
         this.gridSize = 10;
         }
+    getAdjacentTile(tileIndex, direction){
+        //return either the tile location if given both variables
+        //or returns all directions of a tile if just given first variable, will include space you came from
+        //
+        let rowIndex = "";
+        let columnIndex = "";
+        try{
+        if(tileIndex.length === 3){
+        rowIndex = tileIndex.slice(0,2) -1;
+        columnIndex = (tileIndex.charAt(3)).charCodeAt(0) - 97;
+        }else{
+        rowIndex = tileIndex.charAt(0) -1;
+        columnIndex = (tileIndex.charAt(1)).charCodeAt(0) - 97;
+        }
+        switch (direction) {
+            case "up":
+                if(rowIndex == 0) throw "No tile there";
+                return ((rowIndex) + tileIndex.charAt(1));
+                break;
+            case "down":
+                if(rowIndex == this.quadrantArray.length-1) throw "No tile there";
+                return ((rowIndex+2) + tileIndex.charAt(1));
+                break;
+            case "left":
+                if(columnIndex == 0) throw "No tile there";
+                return (tileIndex.charAt(0) + String.fromCharCode(columnIndex+96));
+                break;
+            case "right":
+                if(columnIndex == this.quadrantArray[rowIndex].length-1) throw "No tile there";
+                return (tileIndex.charAt(0) + String.fromCharCode(columnIndex+98));
+                break;
+            
+            default:
+            let arrOfPossibleTiles = [];
+            if (rowIndex != 0) {//up
+                arrOfPossibleTiles.push("up");
+            }
+            if (rowIndex != this.quadrantArray.length-1) {//down
+                arrOfPossibleTiles.push("down");
+            }
+            if (columnIndex != 0) { //left
+                arrOfPossibleTiles.push("left");
+            }
+            if (columnIndex != this.quadrantArray[rowIndex].length-1) {//right
+                arrOfPossibleTiles.push("right");
+            }
+            return arrOfPossibleTiles;
+            //code that excutes without variables
+                break;
+        }//code that excutes with variables
+        }catch(err){
+            console.log(err);
+            
+        }
+
+    }
     generateLevel(level){
+            //minium of 8x8 grid
+        let startTile = "";
+        let moves = [];
+        let lastTile = "";
+        let nextTile = ""
+        switch (level) {
+            case 1:
+                startTile = "1d"
+                moves = ["down", "down", "left", "left", "down", "down", "right", "right", "down", "down", "down"]
+                
+                break;
+            case 2:
+                
+                break;
+            
+            case 3:
+                
+                break;
+            
+            case 4:
+                
+                break;
         
+            case 5:
+                
+                break;
+
+            default:
+                break;
+        }
+        document.getElementById(startTile).classList.add("path");
+        lastTile = startTile;
+        moves.forEach(move => {
+            nextTile = this.getAdjacentTile(lastTile, move);
+            document.getElementById(nextTile).classList.add("path");
+            lastTile = nextTile;
+        })
+        while(this.getAdjacentTile(lastTile).includes("down")) {
+            console.log(this.getAdjacentTile(lastTile).includes("down"));
+            nextTile = this.getAdjacentTile(lastTile, "down");
+            console.log(nextTile);
+            document.getElementById(nextTile).classList.add("path");
+            lastTile = nextTile;
+            
+        }
     }
     gameStart(){
         let introText = "To be written text";
@@ -31,7 +131,6 @@ class GameSesion{
         introBox.open = true;
         // need to work on css for button location
         nextButton.addEventListener("click", (e) => this.mainGame());
-        
     }
     LevelsStart(){
         if(document.querySelectorAll("#centeredDiv > *").length > 0){
@@ -53,18 +152,15 @@ class GameSesion{
                 document.querySelector("tr:last-child").appendChild(element);
             }
         }
-        console.log(this.quadrantArray);
-
-                this.generateLevel(1);
-                // this.startLevel();
-                // this.tutorial(); //hopefully will come back to this
-                
-                if(this.levelWins[0] === true){
-                    this.generateLevel(2);
-                }else{
-                    
-                }
-                console.log("getting here");
+        this.generateLevel(1)
+        // this.startLevel();
+        // this.tutorial(); //hopefully will come back to this
+        
+        // if(this.levelWins[0] === true){
+        //     this.generateLevel(2);
+        // }else{
+            
+        // }
             }
     winnerScreen(){
     }
@@ -73,6 +169,6 @@ class GameSesion{
     }
 }
 let game1 = new GameSesion();
- game1.gameStart();
-// game1.LevelsStart();
+//  game1.gameStart();
+game1.LevelsStart();
 console.log("script loaded");
