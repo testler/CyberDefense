82247e799232fecbd7f025c06a41688c21db5ea5
testler <joshgarst@me.com>
2022-05-01 02:18:20 -0500

    generateLevel complete

--- script.js
+++ script.js
@@ -13,8 +13,9 @@ class GameSesion{
     constructor(){
         this.username = "";
         this.levelWins = [false, false, false, false, false]; //each level is index +1  so level 1 is at index 0
-        this.quadrantArray = [];
+        this.tileArray = [];
         this.gridSize = 10;
+        this.pathArray = [];
         }
     getAdjacentTile(tileIndex, direction){
         //return either the tile location if given both variables
@@ -25,7 +26,7 @@ class GameSesion{
         try{
         if(tileIndex.length === 3){
         rowIndex = tileIndex.slice(0,2) -1;
-        columnIndex = (tileIndex.charAt(3)).charCodeAt(0) - 97;
+        columnIndex = ((tileIndex.charAt(2)).charCodeAt(0) - 97);
         }else{
         rowIndex = tileIndex.charAt(0) -1;
         columnIndex = (tileIndex.charAt(1)).charCodeAt(0) - 97;
@@ -33,18 +34,18 @@ class GameSesion{
         switch (direction) {
             case "up":
                 if(rowIndex == 0) throw "No tile there";
-                return ((rowIndex) + tileIndex.charAt(1));
+                return ((rowIndex) + String.fromCharCode(columnIndex + 97));
                 break;
             case "down":
-                if(rowIndex == this.quadrantArray.length-1) throw "No tile there";
-                return ((rowIndex+2) + tileIndex.charAt(1));
+                if(rowIndex == this.tileArray.length-1) throw "No tile there";
+                return ((rowIndex+2) + String.fromCharCode(columnIndex + 97));
                 break;
             case "left":
                 if(columnIndex == 0) throw "No tile there";
                 return (tileIndex.charAt(0) + String.fromCharCode(columnIndex+96));
                 break;
             case "right":
-                if(columnIndex == this.quadrantArray[rowIndex].length-1) throw "No tile there";
+                if(columnIndex == this.tileArray[rowIndex].length-1) throw "No tile there";
                 return (tileIndex.charAt(0) + String.fromCharCode(columnIndex+98));
                 break;
             
@@ -53,13 +54,13 @@ class GameSesion{
             if (rowIndex != 0) {//up
                 arrOfPossibleTiles.push("up");
             }
-            if (rowIndex != this.quadrantArray.length-1) {//down
+            if (rowIndex != this.tileArray.length-1) {//down
                 arrOfPossibleTiles.push("down");
             }
             if (columnIndex != 0) { //left
                 arrOfPossibleTiles.push("left");
             }
-            if (columnIndex != this.quadrantArray[rowIndex].length-1) {//right
+            if (columnIndex != this.tileArray[rowIndex].length-1) {//right
                 arrOfPossibleTiles.push("right");
             }
             return arrOfPossibleTiles;
@@ -108,26 +109,42 @@ class GameSesion{
             default: throw "reached default switch case on generateLevel"
                 break;
         }
+        //creating enemy path
         document.getElementById(startTile).classList.add("path");
         lastTile = startTile;
+        this.pathArray.push(startTile);
         moves.forEach(move => {
             nextTile = this.getAdjacentTile(lastTile, move);
             document.getElementById(nextTile).classList.add("path");
             lastTile = nextTile;
+            this.pathArray.push(nextTile);
         })
         while(this.getAdjacentTile(lastTile).includes("down")) {
-            console.log(this.getAdjacentTile(lastTile).includes("down"));
             nextTile = this.getAdjacentTile(lastTile, "down");
-            console.log(nextTile);
             document.getElementById(nextTile).classList.add("path");
             lastTile = nextTile;
-            
+            this.pathArray.push(nextTile);
         }
+        //generating the possible tower locations
+        this.tileArray.forEach(arr => {
+            arr.forEach(testTile => {
+                if(document.getElementById(testTile).className.includes("path")){}
+                else{
+                document.getElementById(testTile).classList.add("possibleTowerSpot")
+                }
+            })
+        })
+        
+
     }
     clearGameBoard(){
         while(document.querySelector(".path") != undefined){
             document.querySelector(".path").classList.remove("path")
         }
+        while(document.querySelector(".possibleTowerSpot") != undefined){
+            document.querySelector(".possibleTowerSpot").classList.remove("possibleTowerSpot")
+        }
+        this.pathArray = [];
     }
     gameStart(){
         let introText = "To be written text";
@@ -152,13 +169,13 @@ class GameSesion{
         gameBoard.id = "gameBoard";
         document.querySelector("#centeredDiv").appendChild(gameBoard);
         for (let firstDimensionIndex = 0; firstDimensionIndex < this.gridSize; firstDimensionIndex++) {
-            this.quadrantArray.push([]);
+            this.tileArray.push([]);
             document.querySelector("#gameBoard").appendChild(document.createElement("tr"));
             for (let secondDimensionItem = 0; secondDimensionItem < this.gridSize; secondDimensionItem++) {
-                this.quadrantArray[firstDimensionIndex][secondDimensionItem] = firstDimensionIndex+1 + (String.fromCharCode(secondDimensionItem+97));
+                this.tileArray[firstDimensionIndex][secondDimensionItem] = firstDimensionIndex+1 + (String.fromCharCode(secondDimensionItem+97));
                 let element = document.createElement("td")
-                element.id = (this.quadrantArray[firstDimensionIndex][secondDimensionItem]);
-                element.textContent = (this.quadrantArray[firstDimensionIndex][secondDimensionItem]);
+                element.id = (this.tileArray[firstDimensionIndex][secondDimensionItem]);
+                element.textContent = (this.tileArray[firstDimensionIndex][secondDimensionItem]);
                 document.querySelector("tr:last-child").appendChild(element);
             }
         }
